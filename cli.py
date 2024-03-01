8d16594a1587ba196d606f292ebe14cd238ff660
Kjell Magnus Bjørk <kjmabj@proton.me>
2024-03-01 15:05:27 +0100

    Updated cli.py to work with altered functions.py

--- cli.py
+++ cli.py
@@ -1,32 +1,43 @@
 import functions
 import time
 
-print(f"Welcome to this simple to-do application. Your to-do changes will be written to ./{functions.FILEPATH}")
-print("Will this push?")
+print(f"---\n"
+      f"Welcome to this simple to-do application. Your to-do changes will be written to ./{functions.FILEPATH}\n"
+      f"---\n")
 
 now = time.strftime("%d.%m.%Y %H:%M:%S")
 print(f"The current time is {now}\n")
-print(functions.readme())
+
+todos = functions.get_todos()
+
+print("Initial list of todos:\n")
+for i, index in enumerate(todos):
+    print(f"{i + 1}. {todos[i]}")
+print()
 
 while True:
     user_command = input('Enter command: ')
     user_command = user_command.strip()
 
     if 'add' in user_command:
-        functions.add(user_command)
+        todo = user_command[4:]
+        functions.add_todo(todo)
 
     elif 'edit' in user_command:
-        functions.edit(user_command)
+        index = int(user_command[5:]) - 1
+        replacement_todo = input('Enter replacement todo: ')
+        print(index)
+        print(replacement_todo)
+        functions.replace_todo(index, replacement_todo)
 
     elif 'complete' in user_command:
-        functions.complete(user_command)
+        index = int(user_command[8:]) - 1
+        functions.complete_todo(index)
 
     elif 'show' in user_command:
-        functions.show()
-
-    elif 'help' in user_command:
-        print(functions.readme())
-        functions.readme()
+        todos = functions.get_todos()
+        for i, index in enumerate(todos):
+            print(f"{i+1}. {todos[i]}")
 
     elif 'exit' in user_command:
         break
